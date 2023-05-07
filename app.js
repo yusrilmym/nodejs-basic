76f2636ecf58c63d5610748ae9bc1bffdb2c6c1e
yusrilmym <maulanayusrilmahendra@gmail.com>
2023-05-08 00:14:04 +0700

    true

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-// const firstName = process.argv[2];
-// const lastName = process.argv[3];
- 
-// console.log(`Hello ${firstName} ${lastName}`);
-
-const cpuInformation = process.memoryUsage();
- 
-console.log(cpuInformation);
+const server = new Server({
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : 'dicoding.com',
+});
--- process-object/index.js
+++ process-object/index.js
@@ -1,6 +1,6 @@
 const initialMemoryUsage = 5339648;// TODO 1
 const yourName = process.argv[2];// TODO 2
-const environment = process.argv[0];// TODO 3
+const environment = process.env.NODE_ENV;// TODO 3
  
 for(let i = 0; i <= 10000; i++) {
 // Proses looping ini akan membuat penggunaan memori naik
@@ -11,3 +11,7 @@ const currentMemoryUsage = 5339648;// TODO 4
 console.log(`Hai, ${yourName}`);
 console.log(`Mode environment: ${environment}`)
 console.log(`Penggunaan memori dari ${initialMemoryUsage} naik ke ${currentMemoryUsage}`);
+
+// const server = new Server({
+//     host: process.env.NODE_ENV !== 'development' ? 'localhost' : 'dicoding.com',
+// });
