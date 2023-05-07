dccee6b69d4054a5d3733a0f2834a5cc66cc156a
yusrilmym <maulanayusrilmahendra@gmail.com>
2023-05-07 23:50:38 +0700

    trial

---
+++ app.js
@@ -0,0 +1,8 @@
+// const firstName = process.argv[2];
+// const lastName = process.argv[3];
+ 
+// console.log(`Hello ${firstName} ${lastName}`);
+
+const cpuInformation = process.memoryUsage();
+ 
+console.log(cpuInformation);
---
+++ process-object/index.js
@@ -0,0 +1,13 @@
+const initialMemoryUsage = 5339648;// TODO 1
+const yourName = process.argv[2];// TODO 2
+const environment = process.argv[0];// TODO 3
+ 
+for(let i = 0; i <= 10000; i++) {
+// Proses looping ini akan membuat penggunaan memori naik
+}
+ 
+const currentMemoryUsage = 5339648;// TODO 4
+ 
+console.log(`Hai, ${yourName}`);
+console.log(`Mode environment: ${environment}`)
+console.log(`Penggunaan memori dari ${initialMemoryUsage} naik ke ${currentMemoryUsage}`);
